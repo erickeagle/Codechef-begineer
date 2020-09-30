4e69c500e6ca260b62384aeab10b1551b9fd3c57
T-u-s-h-a-r <68273051+T-u-s-h-a-r@users.noreply.github.com>
2020-10-01 01:07:52 +0530

    Update decrement or increment.cpp

--- decrement or increment.cpp
+++ decrement or increment.cpp
@@ -7,13 +7,13 @@ int main()
     int num;
     cin >> num;
 
-    if ( num % 4 == 0 )
-    {
-        cout << num+1;
+    if ( num % 4 == 0 )    // in this we are checking if the number is divisible by 4 or not
+    {                      // if it is divisible then if() condition is true
+        cout << num+1;     //and num will increase by one (n+1)
     }
-    else
-    {
-        cout << num-1;
+    else                   // otherwise if false then, else block will execute and 
+    {                      // num will decreased by 1
+        cout << num-1;     //(num-1)
     }
  
     return 0;
